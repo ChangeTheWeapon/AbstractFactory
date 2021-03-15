7e69049e1ce89d22c3071288c0e8cdd9fdd2a891
ChangeTheWeapon <gore-veronica@mail.ru>
2021-03-15 16:40:16 +0200

    first commit

---
+++ src/com/company/RoundedRectangle.java
@@ -0,0 +1,7 @@
+package com.company;
+public class RoundedRectangle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Inside RoundedRectangle::draw() method.");
+    }
+}
